fc16722b830daf77f4c998ded913a0c5cf8dbd5e
Vivien B <vivienbihl@gmail.com>
2022-01-20 21:13:22 +0100

    32 bits operations

--- joypad.asm
+++ joypad.asm
@@ -126,3 +126,63 @@ isse2:
 sin_end:
     plp
     rts
+
+
+; 32 bits add
+; input num1: ax(lo), bx(hi)
+; input num2: cx(lo), dx(hi)
+; result in ax(lo), bx(hi)
+Add32:
+    .call RESERVE_STACK_FRAME 04
+    ; 01/02 03/04 -> result
+
+    .call M16
+    ; 32 bits add
+
+    clc
+    lda @ax ; num1_lo
+    adc @cx ; num2_lo
+    sta 01; result_lo
+
+    lda @bx ; num1_hi
+    adc @dx ; num2_hi
+    sta 03 ; result_hi
+
+    lda 01
+    sta @ax
+    lda 03
+    sta @bx
+
+    .call M8
+    .call RESTORE_STACK_FRAME 04
+    rts
+
+; 32 bits LSR
+; @ax -> input lo / result lo
+; @bx -> input hi / result hi
+; @cl -> amount to shift
+Lsr32:
+    .call M16
+    lsr @bx
+    ror @ax
+
+    .call M8
+    dec @cl
+    bne @Lsr32
+
+    rts
+
+; 32 bits ASL
+; @ax -> input lo / result lo
+; @bx -> input hi / result hi
+; @cl -> amount to shift
+Asl32:
+    .call M16
+    asl @ax
+    rol @bx
+
+    .call M8
+    dec @cl
+    bne @Asl32
+
+    rts
