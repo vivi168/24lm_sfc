3092401956afca30fd6b232c71c457f236eb14a8
Vivien B <vivienbihl@gmail.com>
2022-01-22 12:57:26 +0100

    Prepare for update column

--- main.asm
+++ main.asm
@@ -35,7 +35,16 @@ MainLoop:
     jmp @MainLoop
 
 UpdatePlayer:
+    .call RESERVE_STACK_FRAME 04
+    ; 01/02 -> prev player_x
+    ; 03/04 -> prev player_y
+
     .call M16
+
+    lda @player_x
+    sta 01
+    lda @player_y
+    sta 03
 
     ; X coord
     lda @player_fx_lo
@@ -59,11 +68,43 @@ UpdatePlayer:
     lda @ax
     sta @player_y
 
+; ---- Check horizontal offset
+    lda @player_x
+    bit #000f
+    bne @skip_column_update
+
+    cmp 01
+    beq @skip_column_update
+    bcc @going_left_cc
+; going right
+    brk ff
+    ; TODO copy column ahead
+
+    bra @end_column_update
+going_left_cc:
+; going left
+    brk 00
+    ; TODO copy column behind
+
+
+; ---- Check vertical offset
+end_column_update:
+skip_column_update:
+    lda @vertical_offset
+    bit #000f
+    bne @skip_row_update
+
+skip_row_update:
+
+
     .call M8
+    .call RESTORE_STACK_FRAME 04
     rts
 
 CenterCam:
     .call M16
+
+; ---- X coordinate
 
     ; camera.x = player.x - SCREEN_W/2
     lda @player_x
@@ -74,6 +115,8 @@ CenterCam:
     ; screen.x = camera.x % 1024;
     and #03ff
     sta @horizontal_offset
+
+; ---- Y coordinate
 
     ; camera.y = player.y - SCREEN_H/2
     lda @player_y
